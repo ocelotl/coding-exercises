9729be4a08f1cc14d452d70bb51a991f96bb6e08
Diego Hurtado <ocelotl@users.noreply.github.com>
2024-11-05 18:29:33 -0600

    Remove extra list

--- leetcode/001_remove_element.py
+++ leetcode/001_remove_element.py
@@ -4,18 +4,12 @@ from typing import List
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
 
-        result = [0] * len(nums)
-
         k = 0
 
-        for index in range(len(nums)):
-            if nums[index] != val:
-                result[k] = nums[index]
-
+        for num in nums:
+            if num != val:
+                nums[k] = num
                 k += 1
-
-        for index in range(len(result)):
-            nums[index] = result[index]
 
         return k
 
@@ -27,9 +21,9 @@ def test_case():
     nums = [3, 2, 2, 3]
 
     assert solution.removeElement(nums, 3) == 2
-    assert nums == [2, 2, 0, 0]
+    assert nums == [2, 2, 2, 3]
 
     nums = [0, 1, 2, 2, 3, 0, 4, 2]
 
     assert solution.removeElement(nums, 2) == 5
-    assert nums == [0, 1, 3, 0, 4, 0, 0, 0]
+    assert nums == [0, 1, 3, 0, 4, 0, 4, 2]
