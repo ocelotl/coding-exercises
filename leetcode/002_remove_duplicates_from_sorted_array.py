bee139e4933f59b12dfd268571184d91b3482668
Diego Hurtado <ocelotl@users.noreply.github.com>
2024-11-05 18:15:21 -0600

    Add new problem

---
+++ leetcode/002_remove_duplicates_from_sorted_array.py
@@ -0,0 +1,38 @@
+from typing import List
+
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+
+        present = set()
+
+        result = [0] * len(nums)
+
+        k = 0
+
+        for index in range(len(nums)):
+
+            if nums[index] not in present:
+                present.add(nums[index])
+                result[k] = nums[index]
+                k += 1
+
+        for index in range(len(result)):
+            nums[index] = result[index]
+
+        return k
+
+
+def test_case():
+
+    solution = Solution()
+
+    nums = [1, 1, 2]
+
+    assert solution.removeDuplicates(nums) == 2
+    assert nums == [1, 2, 0]
+
+    nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4]
+
+    assert solution.removeDuplicates(nums) == 5
+    assert nums == [0, 1, 2, 3, 4, 0, 0, 0, 0, 0]
